2d7b153bd305432abd999009dd307655e82d2ee0
Kyle Tan <kyle@arist.co>
2025-09-04 12:47:35 -0700

    Fix dependency conflicts

--- scrapy_vectors/__init__.py
+++ scrapy_vectors/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.1.0"
+__version__ = "0.2.1"
 
 from scrapy_vectors.embeddings import EmbeddingsLiteLLMPipeline
 from scrapy_vectors.feedexport import S3VectorsFeedStorage, S3VectorsFeedExporter
